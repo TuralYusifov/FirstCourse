42a00e940c8465c2bf6ddad3dc118423f8957cc3
Tural <turalyusifov92@gmail.com>
2024-03-10 18:01:52 +0400

    Project initially added

---
+++ FirstCourse/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FirstCourse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the first input: ");
+            int var1;
+            while (true)
+            {
+                try
+                {
+                    var1 = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("You can only enter int number");
+                }
+
+            }
+            Console.Write("Enter the second input: ");
+            int var2 = int.Parse(Console.ReadLine());
+            int Sum = sum(var1, var2);
+
+            Console.Write("Sum is: " + Sum);
+
+
+        }
+        public static int sum(int var1, int var2)
+        {
+            return var1 + var2;
+        }
+    }
+    
+}
